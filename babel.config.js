fe8a3c3598d28353395b79465ea96bc37bc05f03
Nicolas Tual <nicolas.tual@outlook.fr>
2023-01-11 02:15:01 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,38 @@
-module.exports = function (api) {
+module.exports = function(api) {
 	api.cache(true)
 	return {
 		presets: ['babel-preset-expo'],
+		plugins: [
+			'nativewind/babel',
+			[
+				'module-resolver',
+				{
+					root: ['./'],
+					extensions: [
+						'.ios.js',
+						'.android.js',
+						'.js',
+						'.jsx',
+						'.json',
+						'.tsx',
+						'.ts',
+						'.native.js',
+					],
+					alias: {
+						'@Action': './src/actions',
+						'@Asset': './src/assets',
+						'@Component': './src/components',
+						'@Container': './src/containers',
+						'@Helper': './src/helpers',
+						'@Hook': './src/hooks',
+						'@Stack': './src/stack/screens.tsx',
+						'@Screen': './src/screens',
+						'@Service': './src/services',
+						'@Store': './src/store',
+						'@Type': './src/types'
+					}
+				}
+			]
+		]
 	}
 }
